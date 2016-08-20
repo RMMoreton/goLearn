8325f75a1606a129f8e502f6d072a3616f1cc88b
RMMoreton <robbie.moreton@gmail.com>
2016-08-20 10:33:43 -0700

    all the basics are here now

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# gopryq
+# gopriq
 
-gopryq implements a priority queue in Go using
+gopriq implements a priority queue in Go using
 [this heap implementation](https://github.com/RMMoreton/goheap) as the backing
 data structure.
+
+[Documentation](https://godoc.org/github.com/RMMoreton/gopriq)
---
+++ gopriq.go
@@ -0,0 +1,33 @@
+// Package gopryq implements a priority queue in Go.
+package gopriq
+
+// I'm using my own implementation of a heap.
+import (
+	"github.com/RMMoreton/goheap"
+)
+
+// This *might* cause some problems down the road. Hopefully not!
+type Sortable goheap.Sortable
+
+// A priority queue is just a structure holding a Heap. I tried to write
+// `type PriQ goheap.Heap` but I couldn't get the type converting right in
+// the functions.
+type PriQ struct {
+	h goheap.Heap
+}
+
+// Add adds a value to the Priority queue.
+func (p *PriQ) Add(e Sortable) {
+	p.h.Add(e)
+}
+
+// Empty reports whether the Priority Queue is empty.
+func (p *PriQ) Empty() bool {
+	return p.h.Empty()
+}
+
+// Remove removes a value from the Priority Queue and returns it. Remove
+// also returns a boolean to indicate if the returned value is valid.
+func (p *PriQ) Remove() (Sortable, bool) {
+	return p.h.Remove()
+}
