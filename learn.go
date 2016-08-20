b29c680035a7be4b8c15072089943cc60950c312
RMMoreton <robbie.moreton@gmail.com>
2016-08-20 14:49:48 -0700

    added learn.go

---
+++ learn.go
@@ -0,0 +1,8 @@
+// Package learn is just here to see if I can force godoc to look
+// into subdirectories.
+package learn
+
+// Why answers the question, "Why should I learn go?"
+func Why() {
+	return "Because you can!"
+}
